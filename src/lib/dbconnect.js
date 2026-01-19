5397a4c75394f829e3aa473f5854e0aebfb4d2a5
HoridasPro <horidassarker6@gmail.com>
2026-01-19 14:14:07 +0600

    final update

--- src/lib/dbconnect.js
+++ src/lib/dbconnect.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "development") {
   clientPromise = global._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  // clientPromise = client.connect();
 }
 
 export default async function connect(collectionName) {
